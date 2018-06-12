45603d4c8071ba967888af30dbef0ae6194d8d57
Unknown <thalysonps3@gmail.com>
2018-06-12 04:21:58 -0400

    new interface for users

--- api/src/App/src/Core/Crud/UsersCrudInterface.php
+++ api/src/App/src/Core/Crud/UsersCrudInterface.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Core\Crud;
 
 use App\Domain\Service\UsersServiceInterface;
 
-interface CrudInterface
+interface UsersCrudInterface
 {
     public function __construct(UsersServiceInterface $usersService);
 }
