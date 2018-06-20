a7cb5d4f34fe4243bf596851a077492740d4510b
Unknown <thalysonps3@gmail.com>
2018-06-19 23:49:46 -0400

    try exception in changepassoword

--- api/src/App/Domain/Handler/User/ChangePassword.php
+++ api/src/App/Domain/Handler/User/ChangePassword.php
@@ -11,7 +11,6 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Firebase\JWT\JWT;
-use Firebase\JWT\ExpiredException;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\Response\RedirectResponse;
 
@@ -42,7 +41,19 @@ final class ChangePassword implements MiddlewareInterface
 
         $data = $request->getParsedBody();
 
-        $token = JWT::decode($token['token'], $this->jwtSecret, ['HS256']);
+        try {
+            $token = JWT::decode($token['token'], $this->jwtSecret, ['HS256']);
+        } catch (\Exception $e) {
+            $error['code'] = '500';
+            $error['message'] = $e->getMessage();
+        }
+
+        if (isset($error['code'])) {
+            return new JsonResponse([
+                'code' => $error['code'],
+                'message' => $error['message']
+            ]);
+        }
 
         $updated = $this->usersService->updatePassword((int) $token->data->id, $data);
 
