76a857d68eef75e4b2ad9773266daef1b0eb2847
Unknown <thalysonps3@gmail.com>
2018-06-23 15:37:36 -0400

    update interface service for css

--- api/src/App/Core/Crud/CssCrudInterface.php
+++ api/src/App/Core/Crud/CssCrudInterface.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace App\Core\Crud;
 
 use App\Domain\Service\CssServiceInterface;
-use App\Domain\Service\UsersServiceInterface;
-use App\Domain\Service\LogsServiceInterface;
 
 interface CssCrudInterface
 {
-    public function __construct(CssServiceInterface $cssService, UsersServiceInterface $usersService, LogsServiceInterface $logsService);
+    public function __construct(CssServiceInterface $cssService);
 }
