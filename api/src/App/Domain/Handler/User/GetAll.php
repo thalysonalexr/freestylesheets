7b9acf1aa969f831aa6ba9624c14e616f9590845
Unknown <thalysonps3@gmail.com>
2018-06-23 15:41:40 -0400

    jla

--- api/src/App/Domain/Handler/User/GetAll.php
+++ api/src/App/Domain/Handler/User/GetAll.php
@@ -10,7 +10,6 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use App\Core\Crud\UsersCrudInterface;
-use Zend\Diactoros\Response\JsonResponse;
 use App\Middleware\TemplateFormatterInterface;
 
 final class GetAll implements MiddlewareInterface, UsersCrudInterface
