98d5481203665b52fb2ebfc4be34e8d3d8b9ae15
Unknown <thalysonps3@gmail.com>
2018-06-25 21:41:52 -0400

    update

--- api/src/App/Infrastructure/Repository/Css.php
+++ api/src/App/Infrastructure/Repository/Css.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Repository;
 
+use App\Domain\Value\CssHistory;
 use App\Domain\Entity\Css as CssEntity;
+use App\Domain\Entity\User as UserEntity;
 
 interface Css
 {
@@ -13,6 +15,8 @@ interface Css
     public function all(array $filters = []): array;
 
     public function findById(int $id): ?CssEntity;
+
+    public function approveStyle(CssHistory $history): bool;
 
     public function edit(CssEntity $user): int;
 
