a7eff57b9740bd446e4d1ca1b776b6ae329604d7
Unknown <thalysonps3@gmail.com>
2018-07-03 21:53:53 -0400

    update webpack

--- api/resources/assets/js/app.js
+++ api/resources/assets/js/app.js
@@ -1,8 +1,11 @@
 const $ = require('jquery');
 window.$ = $;
 window.jQuery = $;
+global.$ = global.jQuery = $;
 
 require('bootstrap');
 require('../css/style.css');
 require('../img/logo.png');
 require('../img/icon-menu.png');
+
+export default {$}
