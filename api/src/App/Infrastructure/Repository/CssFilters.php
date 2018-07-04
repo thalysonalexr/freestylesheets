2b5b5b839a1f29cea01e0d22a21ee9043c182bed
Unknown <thalysonps3@gmail.com>
2018-07-03 21:52:15 -0400

    refactory

--- api/src/App/Infrastructure/Repository/CssFilters.php
+++ api/src/App/Infrastructure/Repository/CssFilters.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Repository;
 
-final class CssFilters
+use App\Infrastructure\Repository\Filters\Filters;
+use App\Infrastructure\Repository\Filters\FiltersInterface;
+
+final class CssFilters extends Filters implements FiltersInterface
 {
     /**
      * @var string
@@ -25,75 +28,11 @@ final class CssFilters
         'offset',
         'order'
     ];
-    /**
-     * @var array
-     */
-    private $cssFilters;
-    /**
-     * @var string
-     */
-    private $statements;
 
     public function __construct(array $filters)
     {
-        $this->cssFilters = self::selectFilters($filters, self::POSSIBLE_FILTERS);
-        $this->statements = self::selectFilters($filters, self::STATEMENTS);
-    }
-
-    public function where(): string
-    {
-        if (empty($this->cssFilters)) {
-            return '';
-        }
-
-        $statement = $this->setStatements();
-
-        $where = ' WHERE ' .
-            implode(' AND ', array_map(function (string $key) {
-                return self::ALIAS . $key . " LIKE :{$key}";
-            }, array_keys($this->cssFilters)));
-
-        return $where . $statement;
-    }
-
-    public function setStatements(): string
-    {
-        $order = explode(',', $this->statements['order']?: '');
-
-        $order = empty($order[0]) ? null : $order;
-
-        $query = (new OrderBy)->values($order, self::ALIAS)->order();
-
-        $query .= (new Pagination)->limit(
-            $this->statements['limit']
-        )->offset(
-            $this->statements['offset']
-        )->pagination();
-
-        return $query;
-    }
-
-    public function data(): array
-    {
-        return $this->cssFilters;
-    }
-
-    public function setLike(): self
-    {
-        $this->cssFilters = array_map(function (string $value) {
-            return '%' . $value . '%';
-        }, $this->cssFilters);
-
-        return $this;
-    }
-
-    public static function selectFilters(array $filters, array $possibles): array
-    {
-        return array_filter(
-            $filters,
-            function (string $key) use ($possibles) {
-                return in_array($key, $possibles);
-            }, ARRAY_FILTER_USE_KEY
-        );
+        $this->alias = self::ALIAS;
+        $this->cssFilters = parent::selectFilters($filters, self::POSSIBLE_FILTERS);
+        $this->statements = parent::selectFilters($filters, self::STATEMENTS);
     }
 }
