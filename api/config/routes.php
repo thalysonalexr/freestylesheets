1865bf922ff50c26b8b626a83091237cb73a9e97
Unknown <thalysonps3@gmail.com>
2018-07-23 00:03:57 -0400

    comment update and add new routes

--- api/config/routes.php
+++ api/config/routes.php
@@ -6,69 +6,54 @@ use Psr\Container\ContainerInterface;
 use Zend\Expressive\Application;
 use Zend\Expressive\MiddlewareFactory;
 
-/**
- * Setup routes with a single request method:
- *
- * $app->get('/', App\Handler\HomePageHandler::class, 'home');
- * $app->post('/album', App\Handler\AlbumCreateHandler::class, 'album.create');
- * $app->put('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.put');
- * $app->patch('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.patch');
- * $app->delete('/album/:id', App\Handler\AlbumDeleteHandler::class, 'album.delete');
- *
- * Or with multiple request methods:
- *
- * $app->route('/contact', App\Handler\ContactHandler::class, ['GET', 'POST', ...], 'contact');
- *
- * Or handling all request methods:
- *
- * $app->route('/contact', App\Handler\ContactHandler::class)->setName('contact');
- *
- * or:
- *
- * $app->route(
- *     '/contact',
- *     App\Handler\ContactHandler::class,
- *     Zend\Expressive\Router\Route::HTTP_METHOD_ANY,
- *     'contact'
- * );
- */
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
 
-    // documentation
+    /* Homepage */
     $app->get('/', \App\Handler\HomePageHandler::class, 'home');
 
+    /* List all docs versions this api */
     $app->get('/api/versions', \App\Handler\VersionsPageHandler::class, 'versions');
 
+    /* List one doc version this api */
     $app->get('/api/versions/{version}', \App\Handler\VersionPageHandler::class, 'version');
 
+    /* Page About of project */
     $app->get('/api/about', \App\Handler\AboutPageHandler::class, 'about');
 
+    /* Page Contact */
     $app->get('/api/contact', \App\Handler\ContactPageHandler::class, 'contact');
 
+    /* Send Contact */
     $app->post('/api/contact', [
         \App\Handler\Middleware\InputFilter\ContactInputFilter::class,
         \App\Handler\Middleware\ContactPostHandler::class
     ], 'contact.post');
 
+    /* Test your ping */
     $app->get('/api/ping', \App\Handler\PingHandler::class, 'api.ping');
 
-    // users
+    /* Create a new user */
     $app->post('/api/v1/users', [
         \App\Middleware\InputFilter\UserInputFilter::class,
         \App\Domain\Handler\User\Create::class
     ], 'user.post');
 
+    /* Generate token for access */
     $app->post('/api/v1/users/login', [
         \App\Middleware\InputFilter\LoginInputFilter::class,
         \App\Domain\Handler\User\Auth::class
     ], 'user.auth.post');
 
+    /* Set timeout */
     $app->post('/api/v1/users/timeout', \App\Domain\Handler\User\Timeout::class, 'user.timeout.post');
 
+    /* Set logout */
     $app->post('/api/v1/users/logout', \App\Domain\Handler\User\Logout::class, 'user.logout.post');
 
+    /* Revoke token of access */
     $app->post('/api/v1/users/revoke-token', \App\Domain\Handler\User\RevokeJwt::class, 'user.revoke.post');
 
+    /* Enable user by id */
     $app->post('/api/v1/users/enable/{id_user}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
@@ -76,6 +61,7 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Domain\Handler\User\Enable::class
     ], 'user.enable.post');
 
+    /* Disable user by id */
     $app->post('/api/v1/users/disable/{id_user}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
@@ -83,17 +69,20 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Domain\Handler\User\Disable::class
     ], 'user.disable.post');
 
-    $app->post('/api/v1/users/change-password', [
-        \App\Middleware\InputFilter\PasswordInputFilter::class,
-        \App\Domain\Handler\User\ChangePassword::class
-    ], 'user.change-password.post');
-
+    /* Init process recovery password */
     $app->post('/api/v1/users/forgot-password', [
         \App\Middleware\InputFilter\EmailInputFilter::class,
         \App\Domain\Handler\User\ForgotPassword::class,
         \App\Middleware\SendMail::class
     ], 'user.forgot-password.post');
 
+    /* Change password by jwt */
+    $app->post('/api/v1/users/change-password', [
+        \App\Middleware\InputFilter\PasswordInputFilter::class,
+        \App\Domain\Handler\User\ChangePassword::class
+    ], 'user.change-password.post');
+
+    /* Get all users */
     $app->get('/api/v1/users', [
         \App\Middleware\CacheMiddleware::class,
         \Middlewares\HttpAuthentication::class,
@@ -106,6 +95,7 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Middleware\HtmlFormatter::class
     ], 'user.all.get');
 
+    /* Get one style by id */
     $app->get('/api/v1/users/{id_user}', [
         \App\Middleware\CacheMiddleware::class,
         \Middlewares\HttpAuthentication::class,
@@ -116,12 +106,14 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Middleware\HtmlFormatter::class
     ], 'user.get');
 
+    /* Edit partial style by id */
     $app->patch('/api/v1/users/{id_user}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
         \App\Domain\Handler\User\Patch::class
     ], 'user.patch');
 
+    /* Edit style by id */
     $app->put('/api/v1/users/{id_user}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
@@ -129,13 +121,14 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Domain\Handler\User\Put::class
     ], 'user.put');
     
+    /* Delete one user by id */
     $app->delete('/api/v1/users/{id_user}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
         \App\Domain\Handler\User\Delete::class
     ], 'user.delete');
 
-    // css
+    /* Create a new style */
     $app->post('/api/v1/css', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
@@ -143,6 +136,7 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Domain\Handler\Css\Create::class
     ], 'css.post');
 
+    /* Approve one style by id */
     $app->post('/api/v1/css/approve/{id_style}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
@@ -150,6 +144,7 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Domain\Handler\Css\Approve::class
     ], 'css.approve.post');
 
+    /* Get all styles */
     $app->get('/api/v1/css', [
         \App\Middleware\CacheMiddleware::class,
         \Middlewares\HttpAuthentication::class,
@@ -161,6 +156,23 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Middleware\HtmlFormatter::class
     ], 'css.all.get');
 
+    /* Get a style by id embedded */
+    $app->get('/api/v1/css/embedded', [
+        // \Middlewares\HttpAuthentication::class,
+        // \App\Middleware\CheckBlacklist::class,
+        \App\Domain\Handler\Css\LinksEmbedded::class,
+        \App\Middleware\JsonFormatter::class,
+        \App\Middleware\XmlFormatter::class,
+        \App\Middleware\HtmlFormatter::class
+    ], 'css.embedded.get');
+
+    /* Direct download of style */
+    $app->get('/api/v1/css/download/{id_style}', [
+        \App\Domain\Handler\Css\Get::class,
+        \App\Domain\Handler\Css\Download::class
+    ], 'css.download.get');
+
+    /* Get a style by id */
     $app->get('/api/v1/css/{id_style}', [
         \App\Domain\Handler\Css\Get::class,
         \App\Middleware\CssFormatter::class,
@@ -170,18 +182,21 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         \App\Middleware\HtmlFormatter::class
     ], 'css.get');
 
+    /* Edit partial one style by id */
     $app->patch('/api/v1/css/{id_style}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
         \App\Domain\Handler\Css\Patch::class
     ], 'css.patch');
 
+    /* Edit one style by id */
     $app->put('/api/v1/css/{id_style}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
         \App\Domain\Handler\Css\Put::class
     ], 'css.put');
 
+    /* Delete one style by id */
     $app->delete('/api/v1/css/{id_style}', [
         \Middlewares\HttpAuthentication::class,
         \App\Middleware\CheckBlacklist::class,
