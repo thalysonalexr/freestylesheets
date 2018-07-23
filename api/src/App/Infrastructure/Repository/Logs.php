1f7d3f7e218c33d11bd6baa35f89f6b80d9a7fdd
Unknown <thalysonps3@gmail.com>
2018-07-23 00:27:36 -0400

    add Jti class in parameters methods

--- api/src/App/Infrastructure/Repository/Logs.php
+++ api/src/App/Infrastructure/Repository/Logs.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 namespace App\Infrastructure\Repository;
 
 use App\Domain\Entity\Log;
+use App\Domain\Value\Jti;
 
 interface Logs
 {
     public function add(Log $log): int;
 
-    public function logout(int $idUser, string $jti): bool;
+    public function logout(int $idUser, Jti $jti): bool;
 
-    public function timeout(int $idUser, string $jti): bool;
+    public function timeout(int $idUser, Jti $jti): bool;
 
-    public function checkTokenInBlacklist(string $jti): bool;
+    public function checkTokenInBlacklist(Jti $jti): bool;
 
-    public function revokeToken(string $jti): bool;
+    public function revokeToken(Jti $jti): bool;
 }
