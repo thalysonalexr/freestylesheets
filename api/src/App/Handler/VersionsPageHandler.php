2253052066e38d24f27714268d750eff74b91881
Unknown <thalysonps3@gmail.com>
2018-07-23 00:26:39 -0400

    add middleware handler for page versions api

---
+++ api/src/App/Handler/VersionsPageHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Template\TemplateRendererInterface;
+
+class VersionsPageHandler implements RequestHandlerInterface
+{
+    /**
+     * @var TemplateRendererInterface
+     */
+    private $template;
+
+    public function __construct(TemplateRendererInterface $template)
+    {
+        $this->template = $template;
+    }
+
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        return new HtmlResponse($this->template->render('pages::versions'));
+    }
+}
