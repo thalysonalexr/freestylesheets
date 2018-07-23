67ff55f550df458a5ce11197659bd4954ab3f699
Unknown <thalysonps3@gmail.com>
2018-07-23 00:04:52 -0400

    update assets file

--- api/resources/assets/js/app.js
+++ api/resources/assets/js/app.js
@@ -1,11 +1,7 @@
 const $ = require('jquery');
-window.$ = $;
-window.jQuery = $;
-global.$ = global.jQuery = $;
 
 require('bootstrap');
 require('../css/style.css');
+require('../js/main.js');
 require('../img/logo.png');
 require('../img/icon-menu.png');
-
-export default {$}
