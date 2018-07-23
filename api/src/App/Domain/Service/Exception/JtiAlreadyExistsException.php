d5deaeceb0914c446720e8da35ce31fb7f615a35
Unknown <thalysonps3@gmail.com>
2018-07-23 00:24:21 -0400

    exception for jti already exists

--- api/src/App/Domain/Service/Exception/JtiAlreadyExistsException.php
+++ api/src/App/Domain/Service/Exception/JtiAlreadyExistsException.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Domain\Service\Exception;
 
+use App\Domain\Value\Jti;
+
 final class JtiAlreadyExistsException extends \DomainException implements ExceptionInterface
 {
-    public static function message(string $jti): self
+    public static function message(Jti $jti): self
     {
-        return new self(sprintf('This "%s" already exists in blacklist', (string) $jti));
+        return new self(sprintf('This "%s" already exists in blacklist', (string) $jti->getValue()));
     }
 }
